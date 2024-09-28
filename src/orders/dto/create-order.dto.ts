a899ddf5addad7e7cfe56f88880307f23169f09a
Christian John jiménez Calvo <christianjjc@mail.com>
2024-09-28 10:51:22 -0500

    sect06-ep08-crear-nueva-orden

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,18 @@
-export class CreateOrderDto {}
+import { IsNumber, IsPositive, IsEnum, IsBoolean, IsOptional } from 'class-validator';
+import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+  @IsNumber()
+  @IsPositive()
+  totalItems: number;
+  @IsEnum(OrderStatusList, {
+    message: `Valid Status are ${OrderStatusList}`,
+  })
+  status: OrderStatus = OrderStatus.PENDING;
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+}
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Post, Body, Param, Inject, Query, ParseIntPipe } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
 import { PaginationDto } from 'src/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ORDER_SERVICE } from 'src/config';
+import { CreateOrderDto } from './dto';
 
 @Controller('orders')
 export class OrdersController {
