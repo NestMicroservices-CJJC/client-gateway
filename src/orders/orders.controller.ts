9d58d32c9f0531a98ca26898e96f456eafde8dda
Christian John jiménez Calvo <christianjjc@mail.com>
2024-09-27 19:34:24 -0500

    sect06-ep05-connect-gateway-orders-ms

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Param, Inject, Query, ParseIntPipe } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { PaginationDto } from 'src/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ORDER_SERVICE } from 'src/config';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(@Inject(ORDER_SERVICE) private readonly orderClient: ClientProxy) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.orderClient.send({ cmd: 'createOrder' }, createOrderDto);
+  }
+
+  @Get()
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.orderClient.send({ cmd: 'findAllOrders' }, paginationDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.orderClient.send({ cmd: 'findOneOrder' }, { id });
+  }
+}
