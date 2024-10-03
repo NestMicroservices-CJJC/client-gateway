d9ed3f50100c5dae75cebb34fe417523959611c0
Christian John jiménez Calvo <christianjjc@mail.com>
2024-10-03 16:43:57 -0500

    sect12-ep09-registrar-usuarios

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { NATS_SERVICE } from 'src/config';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
@@ -9,7 +10,11 @@ export class AuthController {
 
   @Post('auth-register-user')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
-    return this.client.send('auth.register.user', registerUserDto);
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
   }
 
   @Post('auth-login-user')
