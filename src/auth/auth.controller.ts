5d166267ee6b81a29994536f8d817a8e8184a7cc
Christian John jiménez Calvo <christianjjc@mail.com>
2024-10-03 15:20:35 -0500

    sect12-ep06-login-register-DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { NATS_SERVICE } from 'src/config';
 import { ClientProxy } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('auth-register-user')
-  registerUser() {
-    return this.client.send('auth.register.user', {});
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto);
   }
 
   @Post('auth-login-user')
-  loginUser() {
-    return this.client.send('auth.login.user', {});
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto);
   }
 
   @Get('auth-verify-user')
