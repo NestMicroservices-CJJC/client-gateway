3d15e8fe2f6c4c39fa4299882ba9c3d722097777
Christian John jiménez Calvo <christianjjc@mail.com>
2024-10-03 19:38:49 -0500

    FIN SECCION 12

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
 import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { LoginUserDto, RegisterUserDto } from './dto';
 import { catchError } from 'rxjs';
+import { AuthGuard } from './guards';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -26,8 +29,10 @@ export class AuthController {
     );
   }
 
+  @UseGuards(AuthGuard)
   @Get('auth-verify-user')
-  verifyToken() {
-    return this.client.send('auth.verify.user', {});
+  verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    // return this.client.send('auth.verify.user', {});
+    return { user, token };
   }
 }
---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Request } from 'express';
+import { firstValueFrom } from 'rxjs';
+import { NATS_SERVICE } from 'src/config';
+// import { JwtService } from '@nestjs/jwt';
+// import { jwtConstants } from './constants';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Token not found!');
+    }
+    try {
+      const { user, token: newToken } = await firstValueFrom(this.client.send('auth.verify.user', token));
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      request['user'] = user;
+      request['token'] = newToken;
+    } catch {
+      throw new UnauthorizedException('Token not found!');
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
